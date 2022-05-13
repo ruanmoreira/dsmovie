f79e0a3d6cd14d34fdf01b62f5be81e49e17995c
Ruan Moreira <ruaanmoreira2508@gmail.com>
2022-05-13 12:42:32 -0300

    Erro netlify

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,22 +1,22 @@
-import {ReactComponent as GithubIcon} from '../../assets/img/github.svg';
-import './style.css';
+import { ReactComponent as GithubIcon } from '../../assets/img/github.svg';
+import './styles.css';
 
-function Navbar (){
+function Navbar() {
     return (
         <header>
-        <nav className="container">
-            <div className="dsmovie-nav-content">
-                <h1>DSMovie</h1>
-                <a href="https://github.com/ruanmoreira" target="_blank" rel="noreferrer">
-                    <div className="dsmovie-contact-container">
-                        <GithubIcon/>
-                        <p className="dsmovie-contact-link">/ruanmoreira</p>
-                    </div>
-                </a>
-            </div>
-        </nav>
-    </header>
-  );
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/ruanmoreira">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/ruanmoreira</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Navbar;
